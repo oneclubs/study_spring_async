5a2c8eef23c017869c26eac4100ef86c12f40db6
oneclubs <oneclubs@naver.com>
2022-01-13 01:52:01 +0900

    Async - WebAsyncTask

--- src/main/java/com/oneclubs/study/async/controller/AsyncController.java
+++ src/main/java/com/oneclubs/study/async/controller/AsyncController.java
@@ -1,12 +1,14 @@
 package com.oneclubs.study.async.controller;
 
 import com.oneclubs.study.async.app.AsyncService;
+import com.oneclubs.study.common.ThreadUtils;
 import java.util.concurrent.Callable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.WebAsyncTask;
 
 @RestController
 public class AsyncController {
@@ -20,8 +22,20 @@ public class AsyncController {
     Callable<String> getNameCallable() {
         L.info("name callable");
         return () -> {
-            L.info("return callbale");
+            L.info("return callable");
             return "callable";
         };
+    }
+
+    @GetMapping("/name-web-async")
+    WebAsyncTask<String> getNameWebAsync() {
+        L.info("name web async");
+        return new WebAsyncTask<>(
+            1000,
+            () -> {
+                ThreadUtils.sleep(2);
+                return "web async maybe timeout";
+            }
+        );
     }
 }
